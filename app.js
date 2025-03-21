02d2f1387041b53297e093dd135da07be6b89fe6
Rocco Calò <roccocalo03@gmail.com>
2025-03-21 17:56:22 +0100

    delete login and register buttons for logged user

--- app.js
+++ app.js
@@ -33,10 +33,8 @@ passport.use(
       }
       bcrypt.compare(password, user.password, (err, res) => {
         if (res) {
-          // passwords match! log user in
           return done(null, user)
         } else {
-          // passwords do not match!
           return done(null, false, { message: "Incorrect password" })
         }
       })
@@ -64,7 +62,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -77,25 +74,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 const port = process.env.PORT || 3000;
 
-// code for hosting
 app.listen(port, "0.0.0.0", function () {
   console.log(`Server is running on port ${port}`);
 });
